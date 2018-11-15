9d4f839e39e7d73d98290cbf55029306811caa12
Hank Chan <hankchanth@icloud.com>
2018-11-15 15:53:01 -0800

    feat(npmDependencies):
    Add fzf, fzfPreview, print for npmList(); npmListDetails()

---
+++ src/npmDependencies.js
@@ -0,0 +1,113 @@
+/**
+ * List module's dependencies
+ *
+ */
+
+// Dependencies
+const chalk = require('chalk');
+const spawn = require('child_process').spawn;
+const util = require('util');
+
+// Default to node-fzf, the much less capable implementation of fzf, only when fzf is not installed.
+// Using node-fzf will lose many features
+const nfzf = require('node-fzf');
+const commandExists = util.promisify(require('command-exists'));
+
+// Local dependencies
+const {
+	npmDependencies
+} = require('@hankchanocd/npmlist');
+
+
+const npmList = () => {
+	return {
+		fzf() {
+			commandExists('fzf').then(ans => {
+
+				if (ans) {
+					return npmDependencies.npmList().raw()
+						.then(list => {
+							let listStr = list.map(i => chalk.reset(i)).join('\n'); // Use reset to recover the corrupted colored string
+							return spawn(`echo "${listStr}" |
+								fzf --reverse --cycle --ansi --height=40%`, {
+								stdio: 'inherit',
+								shell: true
+							});
+						})
+						.catch(err => console.log(chalk.redBright(err)));
+
+				} else {
+					return defaultNodeFzf();
+				}
+
+			}).catch(err => console.log(err));
+
+		},
+
+
+		fzfPreview() {
+			commandExists('fzf').then(ans => {
+
+				if (ans) {
+					return npmDependencies.npmList().raw()
+						.then(list => {
+							let listStr = list.map(i => chalk.reset(i)).join('\n'); // Use reset to recover the corrupted colored string
+							return spawn(`echo "${listStr}" |
+								fzf --reverse --cycle --ansi \
+	 								--preview-window=70% --preview=" [[ {} = 'Dependencies' ]] || [[ {} = 'DevDependencies' ]] || npm info {} "`, {
+								stdio: 'inherit',
+								shell: true
+							});
+						})
+						.catch(err => console.log(chalk.redBright(err)));
+
+				} else {
+					console.log(`The default node-fzf doesn't have preview option`);
+					return defaultNodeFzf();
+				}
+
+			}).catch(err => console.log(err));
+
+		},
+
+
+		print() {
+			npmDependencies.npmList().default();
+		}
+	};
+};
+
+
+function defaultNodeFzf() {
+	return npmDependencies.npmList().raw()
+		.then(list => {
+			list.forEach(i => {
+				if (i.includes('Dependencies') || i.includes('DevDependencies')) {
+					list.splice(list.indexOf(i), 1);
+				}
+			});
+
+			return nfzf(list, function (result) {
+				const {
+					selected,
+					query
+				} = result;
+				if (!selected) {
+					console.log('No matches for:', query);
+				}
+				console.log(selected.value);
+			});
+		})
+		.catch(err => console.log(chalk.redBright(err)));
+}
+
+
+const npmListDetails = () => {
+	return npmDependencies.npmListDetails();
+};
+
+
+module.exports = {
+	npmList,
+	npmListDetails
+};
