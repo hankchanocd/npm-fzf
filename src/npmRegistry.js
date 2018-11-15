f7e753a1e394ee1ad1c9ffe5e4b75d2831b1565e
Hank Chan <hankchanth@icloud.com>
2018-11-15 13:42:26 -0800

    feat(npmRegistry):
    Add fzf, fzfPreview, print options

---
+++ src/npmRegistry.js
@@ -0,0 +1,102 @@
+/**
+ * Fetch package dependencies and other infos from npm registry
+ *
+ */
+
+// Dependencies
+const chalk = require('chalk');
+const spawn = require('child_process').spawn;
+const util = require('util');
+
+// Default to node-fzf, the much less capable implementation of fzf, only when fzf is not installed.
+// Using node-fzf will lose many features
+const nfzf = require('node-fzf');
+const commandExists = util.promisify(require('command-exists'));
+
+// Local dependencies
+const {
+	npmRegistry
+} = require('@hankchanocd/npmlist');
+
+
+module.exports = (module = '') => {
+	if (!module) {
+		return console.log(chalk.redBright(`Please provide a module`));
+	}
+
+	return {
+		fzf() {
+			commandExists('fzf').then(ans => {
+
+				if (ans) {
+					return npmRegistry(module)
+						.then(i => i.simple().raw())
+						.then(i => spawn(`echo "${i}" | tr ',' '\n' |
+								fzf --reverse --cycle --ansi --height=40%`, {
+							stdio: 'inherit',
+							shell: true
+						}))
+						.catch(err => console.log(chalk.redBright(err)));
+
+				} else {
+					return defaultNodeFzf(module);
+				}
+
+			}).catch(err => console.log(err));
+
+		},
+
+
+		fzfPreview() {
+			commandExists('fzf').then(ans => {
+
+				if (ans) {
+					return npmRegistry(module)
+						.then(i => i.simple().raw())
+						.then(i => spawn(`echo "${i}" | tr ',' '\n' |
+								fzf --reverse --cycle --ansi \
+	 								--preview-window=70% --preview=" echo {} | tr '├──' ' ' | head -n 1 | xargs npm info "`, {
+							stdio: 'inherit',
+							shell: true
+						}))
+						.catch(err => console.log(chalk.redBright(err)));
+
+				} else {
+					console.log(`The default node-fzf doesn't have preview option`);
+					return defaultNodeFzf(module);
+				}
+
+			}).catch(err => console.log(err));
+
+		},
+
+
+		print() {
+			npmRegistry(module)
+				.then(i => i.simple().default())
+				.catch(err => console.log(chalk.redBright(err)));
+		}
+	};
+};
+
+
+function defaultNodeFzf(module = '') {
+	if (!module) {
+		return console.log(chalk.redBright(`Please provide a module`));
+	}
+
+	return npmRegistry(module)
+		.then(i => i.simple().raw())
+		.then(i => nfzf(i, function (result) {
+			const {
+				selected,
+				query
+			} = result;
+			if (!selected) {
+				console.log('No matches for:', query);
+			} else {
+				console.log(selected.value);
+			}
+		}))
+		.catch(err => console.log(chalk.redBright(err)));
+}
